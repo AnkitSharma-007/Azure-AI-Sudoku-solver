6b428b3e791b8af480702dc54f575142506839b4
ankit.sharma <ankit.sharma@extern-hmmh.de>
2021-03-25 15:33:25 +0530

    first commit

---
+++ ngSudokuSolver/ClientApp/src/app/services/form-recognizer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormRecognizerService {
+  baseURL: string;
+
+  constructor(private http: HttpClient) {
+    this.baseURL = '/api/FormRecognizer';
+  }
+
+  getSudokuTableFromImage(image: FormData) {
+    return this.http.post(this.baseURL, image).pipe((response) => {
+      return response;
+    });
+  }
+}
---
+++ ngSudokuSolver/Controllers/FormRecognizerController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ngSudokuSolver.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ngSudokuSolver.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class FormRecognizerController : ControllerBase
+    {
+        private static readonly string endpoint = "FormRecognizer API endpoint";
+        private static readonly string apiKey = "FormRecognizer API key";
+
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<string[][]> Post()
+        {
+            try
+            {
+                string[][] sudokuArray = GetNewSudokuArray();
+
+                if (Request.Form.Files.Count > 0)
+                {
+                    var file = Request.Form.Files[Request.Form.Files.Count - 1];
+
+                    if (file.Length > 0)
+                    {
+                        var memoryStream = new MemoryStream();
+                        file.CopyTo(memoryStream);
+                        byte[] imageFileBytes = memoryStream.ToArray();
+                        memoryStream.Flush();
+
+                        string SudokuLayoutJSON = await GetSudokuBoardLayout(imageFileBytes);
+                        if (SudokuLayoutJSON.Length > 0)
+                        {
+                            sudokuArray = GetSudokuBoardItmes(SudokuLayoutJSON);
+                        }
+                    }
+                }
+
+                return sudokuArray;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        async Task<string> GetSudokuBoardLayout(byte[] byteData)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+            string uri = endpoint + "formrecognizer/v2.1-preview.3/layout/analyze";
+            string LayoutJSON = string.Empty;
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                HttpResponseMessage response;
+                content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    HttpHeaders headers = response.Headers;
+                    IEnumerable<string> values;
+
+                    if (headers.TryGetValues("Operation-Location", out values))
+                    {
+                        string OperationLocation = values.First();
+                        LayoutJSON = await GetJSON(OperationLocation);
+                    }
+                }
+            }
+            return LayoutJSON;
+        }
+
+        async Task<string> GetJSON(string endpoint)
+        {
+            using (var client = new HttpClient(new HttpRetryMessageHandler(new HttpClientHandler())))
+            {
+                var request = new HttpRequestMessage();
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri(endpoint);
+
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+
+                var response = await client.SendAsync(request);
+                var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                return result;
+            }
+        }
+
+        string[][] GetSudokuBoardItmes(string LayoutData)
+        {
+            string[][] sudokuArray = GetNewSudokuArray();
+            dynamic array = JsonConvert.DeserializeObject(LayoutData);
+            int countOfCells = ((JArray)array?.analyzeResult?.pageResults[0]?.tables[0]?.cells).Count;
+
+            for (int i = 0; i < countOfCells; i++)
+            {
+                int rowIndex = array.analyzeResult.pageResults[0].tables[0].cells[i].rowIndex;
+                int columnIndex = array.analyzeResult.pageResults[0].tables[0].cells[i].columnIndex;
+
+                sudokuArray[rowIndex][columnIndex] = array.analyzeResult.pageResults[0].tables[0].cells[i]?.text;
+            }
+            return sudokuArray;
+        }
+
+        string[][] GetNewSudokuArray()
+        {
+            string[][] sudokuArray = new string[9][];
+
+            for (int i = 0; i < 9; i++)
+            {
+                sudokuArray[i] = new string[9];
+            }
+
+            return sudokuArray;
+        }
+    }
+}
